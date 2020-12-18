88d894424f1cac8b392915bb4e6eaa693c0f4b3a
williamowen65 <william.owen.bsc@gmail.com>
2020-12-18 12:39:12 -0800

    commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum voluptatibus quasi quas recusandae sint, ea nobis fuga, dicta inventore laborum, temporibus sed porro doloremque. Blanditiis, quasi. Iste assumenda atque vero corporis dolorem quos ipsum? Laborum ratione quasi neque eligendi similique natus? Facere dolorem repudiandae sapiente fugit possimus? Amet, excepturi rem doloremque eius assumenda odit hic nulla, et mollitia odio natus repudiandae ea magni. Exercitationem fugit ullam perspiciatis voluptatibus. Porro itaque quia, magnam optio dolor nulla numquam fuga quo distinctio sit animi deserunt molestias saepe odio quam! Aspernatur ullam sapiente culpa at impedit ab repellendus odit exercitationem. Illum est assumenda fugiat.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum voluptatibus quasi quas recusandae sint, ea nobis fuga, dicta inventore laborum, temporibus sed porro doloremque. Blanditiis, quasi. Iste assumenda atque vero corporis dolorem quos ipsum? Laborum ratione quasi neque eligendi similique natus? Facere dolorem repudiandae sapiente fugit possimus? Amet, excepturi rem doloremque eius assumenda odit hic nulla, et mollitia odio natus repudiandae ea magni. Exercitationem fugit ullam perspiciatis voluptatibus. Porro itaque quia, magnam optio dolor nulla numquam fuga quo distinctio sit animi deserunt molestias saepe odio quam! Aspernatur ullam sapiente culpa at impedit ab repellendus odit exercitationem. Illum est assumenda fugiat.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {  }
 }
 </script>
