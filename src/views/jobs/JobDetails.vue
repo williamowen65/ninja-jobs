0b50852fc85fddfdaac59d81dca78266360c5eb6
williamowen65 <william.owen.bsc@gmail.com>
2020-12-19 14:28:22 -0800

    commit for jobs project

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,15 +1,29 @@
 <template>
   <h1>Job Details Page {{ id }} </h1>
+    <div v-if="job">
+      <p> {{ job.title }} </p>
+      <p> {{ job.details }} </p>
+    </div>
+    <div v-else>
+      Loading page ....
+    </div>
 </template>
 
 <script>
 export default {
     props: ['id'],
-    // data() {
-    //     return {
-    //         id: this.$route.params.id
-    //     }
-    // }
+    data() {
+        return {
+            // alt way to directly look in url for data
+            // id: this.$route.params.id
+          job: null
+        }
+    },
+    mounted() {
+      fetch(' http://localhost:3000/jobs/' + this.id)
+        .then(res => res.json())
+        .then(data => this.job = data)
+    }
 }
 </script>
 
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,9 +1,14 @@
     <template>
   <h1>Jobs in the market</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-     <router-link :to="{ name: 'JobDetails', params: { id: job.id }}">
-          <h2> {{ job.title }} </h2>
-     </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+        <router-link :to="{ name: 'JobDetails', params: { id: job.id }}">
+            <h2> {{ job.title }} </h2>
+        </router-link>
+    </div> 
+  </div>
+  <div v-else>
+      Loading Page . . .
   </div>
 </template>
 
@@ -11,12 +16,14 @@
 export default {
     data() {
         return {
-            jobs: [
-                {title: "Ninja UX Designer", id: "mario", details: "lorem"},
-                {title: "Ninja Web Developer", id: 2, details: "lorem"},
-                {title: "Ninja Vue Developer", id: "HelloWorld", details: "lorem"}
-            ]
+            jobs: []
         }
+    },
+    mounted() {
+        fetch(' http://localhost:3000/jobs')
+            .then(res => res.json())
+            .then(data => this.jobs = data)
+            .catch(err => console.log(err))
     }
 }
 </script>
